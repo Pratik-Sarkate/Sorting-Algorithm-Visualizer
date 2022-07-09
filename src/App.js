dd7c5fc7a18beb3dab39300768a0b276fb157e01
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2022-07-09 14:35:15 +0530

    Visualizer project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SortingComponent from './Components/Visualiser/sortingComponent';
+
+function App() {
+	return (
+		<div className="App">
+			<SortingComponent />
+		</div>
+	);
+}
+
+export default App;
+	
